1ca379db01a070e55fce71b29db34f6e3420625f
Elisberto Junior <elisberto_jr@hotmail.com>
2022-11-27 02:32:54 -0300

    estilizando a home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BannerComponent } from './components/banner/banner.component';
+
+import { TechnologiesComponent } from './components/technologies/technologies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    BannerComponent,
+    TechnologiesComponent
   ],
   imports: [
     BrowserModule,
