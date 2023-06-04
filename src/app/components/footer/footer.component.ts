f79c9aa1d24ee1743cbde2797d1d7f47c5430eaf
Elisberto Junior <elisberto_jr@hotmail.com>
2023-06-04 19:19:27 -0300

    feat: add current year

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  currentYear: number = new Date().getFullYear()
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+
   }
 
 }
